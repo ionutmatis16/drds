f4650f460eb01bbceabcd82e0351f4f7af426870
Matis Ionut <ionutmatis16@yahoo.com>
2020-03-26 21:12:09 +0200

    Added IPFS

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,73 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
+import React, {Component} from 'react';
 import './App.css';
 
+// TODO: change favicon?
+const ipfsClient = require('ipfs-http-client');
+
+// connect to ipfs daemon API server
+const ipfs = ipfsClient('http://localhost:5001');
+const ipfsPath = "https://localhost:5001/ipfs/";
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            buffer: null,
+            ipfsHash: null
+        };
+    }
+
+    captureFile = (event) => {
+        event.preventDefault();
+        const uploadedFile = event.target.files[0];
+        const reader = new window.FileReader(); // to convert into buffer to be put on IPFS
+        reader.readAsArrayBuffer(uploadedFile);
+        reader.onloadend = () => {
+            this.setState({
+                buffer: Buffer(reader.result)
+            })
+        }
+    };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+        console.log("submitting");
+        for await (const file of ipfs.add(this.state.buffer)) {
+            this.setState({ipfsHash: file.path})
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <span
+              className="navbar-brand col-sm-3 col-md-2 mr-0">
+            DRDS
+          </span>
+                </nav>
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        <main role="main" className="col-lg-12 d-flex text-center">
+                            <div className="content mr-auto ml-auto">
+                                <h1>Data Reliability using Decentralized Systems</h1>
+                                <img src={(ipfsPath + this.state.ipfsHash)} alt="Ipfs image hash"/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <h2>Upload a file</h2>
+                                <form onSubmit={this.onSubmit}>
+                                    <input type="file" onChange={this.captureFile}/>
+                                    <input type="submit"/>
+                                </form>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
