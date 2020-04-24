aafd02aa16b127b4d07e3c3d95930cb84e8eef4f
Matis Ionut <ionutmatis16@yahoo.com>
2020-04-24 22:13:55 +0300

    - added file corruption detection

---
+++ src/components/dumb/InvalidFileIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import ReactTooltip from "react-tooltip";
+
+const InvalidFileIcon = () => (
+    
+);
+
+export default InvalidFileIcon;
